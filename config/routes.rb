02b1fc1d437ddd9c78b4aced4a5c1bcaccb162b1
ariticas10 <ariticas10@gmail.com>
2021-04-15 16:08:16 -0700

    first

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/services'
+  get 'static_pages/blog'
+  get 'static_pages/contact'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
